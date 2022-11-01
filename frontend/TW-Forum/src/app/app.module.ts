8b12a8e9a6235b712e720e4a1c496a6b5f3e0c5d
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2022-11-01 18:37:15 +0200

    Home screen design

--- frontend/TW-Forum/src/app/app.module.ts
+++ frontend/TW-Forum/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatIconModule} from '@angular/material/icon'
-import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/TW-Forum/src/mock-tags.ts
@@ -0,0 +1,9 @@
+import { Tag } from "./tag";
+
+export const TAGS:Tag[]=[
+    {tag_id:1,tag_name:'UVT'},
+    {tag_id:1,tag_name:'Politehnica'},
+    {tag_id:1,tag_name:'Medicina'},
+    {tag_id:1,tag_name:'UPT'},
+    {tag_id:1,tag_name:'Oxford'},
+]
