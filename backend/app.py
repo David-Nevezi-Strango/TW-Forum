359406ec46b7b244f2956968c4ba7239b5932bb7
David-Nevezi-Strango <92172532+David-Nevezi-Strango@users.noreply.github.com>
2022-12-11 18:05:11 +0200

    First try of backend

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,40 @@ from flask_restful import Resource, Api, abort, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+discussion_put_args = reqparse.RequestParser()
+discussion_put_args.add_argument("title", type=str, help="Title of the discussion is required", required=True)
 
+class Discussion(Resource):
+    def get(self, discussion_id):
+        #sql
+        pass
+    def put(self):
+        args = discussion_put_args.parse_args()
+        discussion = args
+        #sql
+        return 0
+
+comment_put_args = reqparse.RequestParser()
+comment_put_args.add_argument("text", type=str, help="Text of the comment is required", required=True)
+
+class Comment(Resource):
+    def get(self, comment_id):
+        return {"hello":"there"}
+    def put(self, comment_id):
+        #request.form
+        args = comment_put_args.parse_args()
+        print(args)
+        #sql
+        return args
+
+class Preferences(Resource):
+    def get(self, user_id):
+        #sql
+        return
+
+#class Tags(Resource):
+
+#class User(Resource):
 
 
 class ResourceHandler(Resource):
@@ -12,6 +45,8 @@ class ResourceHandler(Resource):
         return {"Hello":"World!"}
 
 api.add_resource(ResourceHandler, "/")
+api.add_resource(Discussion, "/discussion")
+api.add_resource(Comment, "/comment/<int:comment_id>")
 
 @app.route('/')
 def hello_world():  # put application's code here
--- backend/test.py
+++ backend/test.py
@@ -3,5 +3,7 @@ import requests
 
 base = "http://127.0.0.1:5000/"
 
-response = requests.get(base)
+response = requests.put(base + "comment/6" , {"text":"ey"})
+print(response)
+response = requests.get(base + "comment/6")
 print(response.json())
