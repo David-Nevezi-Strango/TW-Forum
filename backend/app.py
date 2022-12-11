75acdf76a0c32b76a747df9dffb2d4953a98acd3
David-Nevezi-Strango <92172532+David-Nevezi-Strango@users.noreply.github.com>
2022-12-11 16:44:36 +0200

    configure intellij onto frontend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api, abort, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
+
+
+
+class ResourceHandler(Resource):
+    def get(self):
+        return {"Hello":"World!"}
+
+api.add_resource(ResourceHandler, "/")
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
