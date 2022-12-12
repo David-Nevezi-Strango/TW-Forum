1cd70ec8a25ab8817d9eab592a330a993eb4bede
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2022-12-12 20:01:50 +0200

    Discussion screen

--- frontend/TW-Forum/src/app/discussion-detail/discussion-detail.component.ts
+++ frontend/TW-Forum/src/app/discussion-detail/discussion-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DiscussionService } from '../services/discussion.service';
 import { Discussion } from 'src/models/Discussion';
+import { CommentService } from '../services/comment.service';
+import { Comment } from 'src/models/Comment';
 
 @Component({
   selector: 'app-discussion-detail',
@@ -11,9 +13,9 @@ import { Discussion } from 'src/models/Discussion';
 })
 export class DiscussionDetailComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute,private location: Location,private discussionService:DiscussionService) { }
+  constructor(private route: ActivatedRoute,private location: Location,private discussionService:DiscussionService,private commentService:CommentService) { }
   discussion:Discussion|undefined
-
+  comments:Comment[]=[]
 
   ngOnInit(): void {
     this.getDiscussion()
@@ -23,8 +25,13 @@ export class DiscussionDetailComponent implements OnInit {
     const id=Number(this.route.snapshot.paramMap.get('id'));
     this.discussionService.getDiscussion(id).subscribe(discussion=>{
       this.discussion=discussion;
-      console.log(this.discussion)
+      this.getComments(this.discussion.discussion_id)
     })
   }
 
+  getComments(id:number):void{
+    this.commentService.getComments(id).subscribe(comments=>{
+      this.comments=comments;
+    })
+  }
 }
--- frontend/TW-Forum/src/app/services/comment.service.ts
+++ frontend/TW-Forum/src/app/services/comment.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Comment } from 'src/models/Comment';
+import { COMMENTS } from 'src/mock-data/mock-comments';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,14 @@ import { Injectable } from '@angular/core';
 export class CommentService {
 
   constructor() { }
+
+  getComments(id:number):Observable<Comment[]>{
+    let result:Comment[]=[]
+    for(let comment of COMMENTS){
+      if(comment.discussion_id==id){
+        result.push(comment);
+      }
+    }
+    return of(result)
+  }
 }
