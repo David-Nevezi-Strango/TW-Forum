d7db2b1616d0a4f00b0f11647d93eca52c5d38f7
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2023-01-04 15:38:17 +0200

    Get tags

--- frontend/TW-Forum/src/app/services/tag.service.ts
+++ frontend/TW-Forum/src/app/services/tag.service.ts
@@ -12,15 +12,15 @@ export class TagService {
   constructor(private http: HttpClient) { }
   url="localhost:5000/tags"
 
-  /*
   getTags():Observable<Tag[]>{
     return this.http.get<Tag[]>(this.url)
   }
-  */
 
+  /*
   getTags():Observable<Tag[]>{
     return of(TAGS)
   }
+  */
 
   getTag(id:number):Observable<Tag>{
     for(let tag of TAGS){
