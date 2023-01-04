14498a4bbe679efdb0902821339cdc3d05a1c9b8
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2023-01-04 20:42:38 +0200

    POST

--- frontend/TW-Forum/src/app/add-discussion/add-discussion.component.ts
+++ frontend/TW-Forum/src/app/add-discussion/add-discussion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DiscussionService } from '../services/discussion.service';
 
 @Component({
   selector: 'app-add-discussion',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDiscussionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private discussionService:DiscussionService) { }
+
+  title:string=""
+  tag:string=""
+  description:string=""
 
   ngOnInit(): void {
+  }
+
+  addDiscussion(){
+    let discussion={"title":this.title,"tag_name":this.tag,"description":this.description}
+    this.discussionService.addDiscussion(discussion).subscribe()
+    console.log("function called")
   }
 
 }
